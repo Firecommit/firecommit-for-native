fa0df81f51049176ab505a85af79f7dd7e3d9953
maturu <maturu.410@gmail.com>
2021-11-19 14:57:22 +0900

    feat(screen): add screen component like signin, signup, map, join, user.

---
+++ src/components/SignUpScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export const SignUpScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>SignUp Page</Text>
+    </View>
+  );
+};
---
+++ src/components/UserScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export const UserScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>User Page</Text>
+    </View>
+  );
+};
