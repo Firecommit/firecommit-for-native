35c5689d9d4e991ebe1905c4a8679aabaa691a40
maturu <maturu.410@gmail.com>
2021-11-29 19:06:10 +0900

    feat(firebase): add firebase sdk config

---
+++ firebase.ts
@@ -0,0 +1,31 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DB_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MSG_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} from '@env';
+
+const config = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DB_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MSG_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
+};
+
+const app = firebase.initializeApp(config);
+const auth = firebase.auth(app);
+const db = firebase.database(app);
+
+export default firebase;
+export { auth, db };
