8eff84858b5a45c5bd1cc17a0865eb31d6d647bb
maturu <maturu.410@gmail.com>
2021-12-20 18:25:45 +0900

    ユーザーページの実装と認証データの更新

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,11 +1,141 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Headline } from 'react-native-paper';
+import React, { useContext, useState } from 'react';
+import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button, List, TextInput } from 'react-native-paper';
+import { AuthContext } from '../contexts/AuthProvider';
 
 export const UserScreen = () => {
+  const { currentUser, update } = useContext(AuthContext);
+  const [name, setName] = useState<string>(`${currentUser.auth?.displayName}`);
+  const [email, setEmail] = useState<string>(`${currentUser.auth?.email}`);
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState({
+    name: false,
+    email: false,
+    password: false,
+  });
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Headline>User Page</Headline>
-    </View>
+    <ScrollView>
+      <List.Section>
+        <List.Subheader>アカウント</List.Subheader>
+        <View style={{ backgroundColor: '#fff' }}>
+          <List.Accordion
+            title="ユーザー名設定"
+            style={{
+              backgroundColor: '#fff',
+            }}
+          >
+            <View
+              style={{
+                padding: 16,
+                borderBottomWidth: 3,
+                borderColor: '#eee',
+              }}
+            >
+              <TextInput
+                label="氏名"
+                mode="flat"
+                value={name}
+                autoCapitalize="none"
+                onChangeText={(text) => setName(text)}
+                style={{ marginBottom: 16 }}
+              />
+              <Button
+                mode="contained"
+                loading={loading.name}
+                onPress={() => {
+                  setLoading((l) => ({ ...l, name: true }));
+                  update('name', name).then(() => {
+                    setTimeout(
+                      () => setLoading((l) => ({ ...l, name: false })),
+                      1000
+                    );
+                  });
+                }}
+              >
+                更新
+              </Button>
+            </View>
+          </List.Accordion>
+          <List.Accordion
+            title="メールアドレス設定"
+            style={{
+              backgroundColor: '#fff',
+            }}
+          >
+            <View
+              style={{
+                padding: 16,
+                borderBottomWidth: 3,
+                borderColor: '#eee',
+              }}
+            >
+              <TextInput
+                label="メールアドレス"
+                mode="flat"
+                value={email}
+                autoCapitalize="none"
+                onChangeText={(text) => setEmail(text)}
+                style={{ marginBottom: 16 }}
+              />
+              <Button
+                mode="contained"
+                loading={loading.email}
+                onPress={() => {
+                  setLoading((l) => ({ ...l, email: true }));
+                  update('email', email).then(() => {
+                    setTimeout(
+                      () => setLoading((l) => ({ ...l, email: false })),
+                      1000
+                    );
+                  });
+                }}
+              >
+                更新
+              </Button>
+            </View>
+          </List.Accordion>
+          <List.Accordion
+            title="パスワード設定"
+            style={{
+              backgroundColor: '#fff',
+            }}
+          >
+            <View
+              style={{
+                padding: 16,
+                borderBottomWidth: 3,
+                borderColor: '#eee',
+              }}
+            >
+              <TextInput
+                label="メールアドレス"
+                mode="flat"
+                value={password}
+                autoCapitalize="none"
+                onChangeText={(text) => setPassword(text)}
+                style={{ marginBottom: 16 }}
+              />
+              <Button
+                mode="contained"
+                loading={loading.password}
+                onPress={() => {
+                  setLoading((l) => ({ ...l, password: true }));
+                  update('password', password).then(() => {
+                    setTimeout(
+                      () => setLoading((l) => ({ ...l, password: false })),
+                      1000
+                    );
+                  });
+                }}
+              >
+                更新
+              </Button>
+            </View>
+          </List.Accordion>
+        </View>
+      </List.Section>
+    </ScrollView>
   );
 };
