e26211d6675e2cbe93a59593a22a0dcb38e370aa
maturu <maturu.410@gmail.com>
2022-05-23 01:12:10 +0900

    refacture(user): fix updagePicture api

--- src/features/users/api/updatePicture.ts
+++ src/features/users/api/updatePicture.ts
@@ -1,4 +1,4 @@
-import firebase, {db} from '&/lib/firebase';
+import firebase, {db, storage} from '&/lib/firebase';
 
 export type UpdatePictureDTO = {
   user: firebase.User;
@@ -6,7 +6,11 @@ export type UpdatePictureDTO = {
 };
 
 export const updatePicture = async (data: UpdatePictureDTO) => {
-  await data.user.updateProfile({photoURL: data.value});
+  const blob = await fetch(data.value).then(res => res.blob());
+  const imageRef = storage.ref('users/icon').child(data.user.uid);
+  const snapshot = await imageRef.put(blob);
+  const url = await snapshot.ref.getDownloadURL();
+  await data.user.updateProfile({photoURL: url});
   const userRef = db.ref('users').child(data.user.uid);
-  await userRef.update({photoURL: data.value});
+  await userRef.update({photoURL: url});
 };
