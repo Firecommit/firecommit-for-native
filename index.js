19d18dbcc5f2bca750eb41bd8f03aac9784dc2ea
maturu <maturu.410@gmail.com>
2022-06-06 16:18:00 +0900

    chore(package): add gesture-handler, reanimated, drawer package

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry, LogBox} from 'react-native';
 import {App} from '&/App';
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
 
 /**
  * Ignore the "setState on unmounted component" Warning in React ^18
